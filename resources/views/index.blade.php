54a5047a27c8275994bd495eed58f4d5629ff42d
Ryan Bibby <bibby.ryan@gmail.com>
2020-10-24 12:31:34 +0100

    Homepage design done

---
+++ resources/views/index.blade.php
@@ -0,0 +1,60 @@
+<html>
+    <head>
+        <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700&display=swap" rel="stylesheet">
+
+        <title>Journey Planner</title>
+
+        <style>
+            body {
+                font-family: 'Nunito';
+            }
+        </style>
+    </head>
+
+    <body class="bg-cover" style="background-image: url('{{ asset('img/background2.jpg') }}'">
+        <div class="h-screen w-screen flex items-center justify-center">
+
+            <div class="bg-white shadow-lg p-8 mx-8 w-full md:w-1/4">
+                <h1 class="text-3xl font-light mb-4 text-gray-800">Journey Planner</h1>
+
+                <form action="{{ route('calculate-route') }}" method="POST">
+                    {{ csrf_field() }}
+                    <div class="block mb-4">
+                        <label for="start" class="block text-sm text-gray-800 mb-1">Start:</label>
+                        <input type="text" name="start" class="w-full bg-gray-100 border border-gray-500 py-2 px-4 rounded focus:border-2 focus:border-gray-500 appearance-none outline-none">
+                    </div>
+                   
+                    <div class="block mb-4">
+                        <label for="destination" class="block text-sm text-gray-800 mb-1">Destination:</label>
+                        <input type="text" name="destination" class="w-full bg-gray-100 border border-gray-500 py-2 px-4 rounded focus:border-2 focus:border-gray-500 appearance-none outline-none">
+                    </div>
+
+                    <div class="block mb-4">
+                        <label for="" class="block text-sm text-gray-800 mb-1">Stop every:</label>
+                        <div class="flex flex-wrap">
+                            <select name="hours" id="" class="w-full md:w-1/2 bg-gray-100 border border-gray-500 py-2 px-4 rounded-l focus:border-2 focus:border-gray-500 appearance-none outline-none">
+                            @for($i = 0; $i <= 4; $i++)
+                                <option value="{{ $i }}" @if(old('hours', 2) == $i) selected @endif>{{ $i }} {{ Str::plural('hour', $i) }}</option>
+                            @endfor
+                            </select>
+
+                            <select name="minutes" id="" class="w-full md:w-1/2 bg-gray-100 border-t border-r border-b border-gray-500 py-2 px-4 rounded-r focus:border-2 focus:border-gray-500 appearance-none outline-none">
+                            @foreach([0, 15, 30, 45] as $i)
+                                <option value="{{ $i }}">{{ $i }} {{ Str::plural('minute', $i) }}</option>
+                            @endforeach
+                            </select>
+                        </div>
+                        <p class="text-xs text-light text-gray-700 mt-2">
+                            It is advised you take a 15 minute break after every 2 hours of driving
+                        </p>
+                    </div>
+
+                    <div class="flex justify-end">
+                        <button class="text-lg font-light text-white bg-indigo-700 border border-indigo-800 w-full md:w-auto px-4 py-2 rounded hover:shadow-lg hover:bg-indigo-800" type="submit">Plan Route ></button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\PageController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +15,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PageController::class, 'index']);
+Route::post('/', [PageController::class, 'submit'])->name('calculate-route');
